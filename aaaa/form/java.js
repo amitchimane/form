7651730cf2ba777ee2c2e3248c1973623ef62e73
amit chimane <amitchimane85@gmail.com>
2017-01-09 15:13:41 +0700

    first commit

---
+++ aaaa/form/java.js
@@ -0,0 +1,21 @@
+/*function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}*/
+
+ <script>        
+           function phoneno(){          
+            $('#phone').keypress(function(e) {
+                var a = [];
+                var k = e.which;
+
+                for (i = 48; i < 58; i++)
+                    a.push(i);
+
+                if (!(a.indexOf(k)>=0))
+                    e.preventDefault();
+            });
+        }
+       </script>
